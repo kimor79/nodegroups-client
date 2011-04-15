b633f02018217d907d819086db3ae6fd5595344c
Kimo Rosenbaum <kimor79@yahoo.com>
2011-04-15 16:05:04 -0700

    phpdoc changes

--- php/classes/client.php
+++ php/classes/client.php
@@ -1,6 +1,6 @@
 <?php
 
-/**
+/*
 
 Copyright (c) 2010, Kimo Rosenbaum and contributors
 All rights reserved.
@@ -27,7 +27,17 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-**/
+*/
+
+/**
+ * client.php
+ * @package NodegroupsClient
+ */
+
+/**
+ * NodegroupsClass
+ * @package NodegroupsClient
+ */
 
 class NodegroupsClient {
 
